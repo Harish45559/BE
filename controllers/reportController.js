ad2041c8b26f50d987f24970cb49e0b014a048a3
Harish Lenkalapalli <dme.harishguptha@gmail.com>
2025-06-12 15:49:34 +0530

    fixed

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -72,12 +72,6 @@ function minutesToHoursMinutes(minutes) {
 }
 
 
-function minutesToHoursMinutes(minutes) {
-  const h = Math.floor(minutes / 60);
-  const m = Math.floor(minutes % 60);
-  return `${h.toString().padStart(2, '0')}:${m.toString().padStart(2, '0')}`;
-}
-
 
 exports.deleteAttendance = async (req, res) => {
   try {
