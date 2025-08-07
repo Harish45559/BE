3e6f9f2283cefb423d26f5418c5421fd8a12b0c8
Harish Lenkalapalli <dme.harishguptha@gmail.com>
2025-08-07 20:11:21 +0530

    ff

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,12 @@
+// routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 
+// Login route
 router.post('/login', authController.login);
+
+// Forgot password route
 router.post('/forgot-password', authController.forgotPassword);
 
 module.exports = router;
